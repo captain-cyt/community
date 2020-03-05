95197864cf3514d5f467e96d1c8fc9393751c1cf
captain-cyt <756059230@qq.com>
2020-03-05 14:35:55 +0800

    'README.md'

---
+++ README.md
@@ -0,0 +1,9 @@
+## 码匠社区
+## 资料
+https://spring.io
+https://spring.io/guides/gs/serving-web-content/
+https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys
+https://elasticsearch.cn/explore
+## 工具
+https://git-scm.com/"# community" 
+"# community" 
---
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -0,0 +1,20 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author c_sir
+ * @create 2020-03-05 12:48
+ */
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model modle){
+        modle.addAttribute("name", name);
+    return "hello"; //这里的hello必须是hello 其他的都不行，因为resuorces/template下面的HTML文件名是hello
+    }
+}
